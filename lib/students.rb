69c294bbae6dfca1010dec005e95c1209f3f51f1
Shahwar Spall <shevy.spall@gmail.com>
2020-01-24 03:25:48 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,13 +5,17 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
+  "select min(gpa) FROM students"
 end
 
 def average_student_gpa
+  "select avg(gpa) FROM students"
 end
 
 def total_tardies_for_all_students
+  "select SUM(tardies) FROM students"
 end
 
 def average_gpa_for_9th_grade
+  "select avg(gpa) FROM students where grade is 9"
 end
